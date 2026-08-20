00b48f228021c513b0876c6390dcdc152413d208
sakupapa <otakama@gmail.com>
2026-08-21 07:56:50 +0900

    renew

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
+docker pull quay.io/frrouting/frr:10.5.3
 docker build -t vnc-desktop:bookworm -f Dockerfile.vnc . 
 docker build -t nginx:1.0 -f Dockerfile.nginx .
-docker build -t coredns:1.0 -f Dockerfile.coredns .
